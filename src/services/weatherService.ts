a18868e2a41eab21990ad14cb82d51fe61403c13
Valentin Nelu Ifrim <nelu@valentins-macbook-pro.local>
2024-06-27 08:59:58 +0200

    fix: remove unused functions

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -1,15 +1,5 @@
-const BASE_URL_CURRENT = `https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_WEATHER_API_KEY}`;
 const BASE_URL_FORECAST = `https://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_WEATHER_API_KEY}`;
 const BASE_URL_SEARCH = `https://api.weatherapi.com/v1/search.json?key=${import.meta.env.VITE_WEATHER_API_KEY}`;
-
-export const getCurrentAuto = () => {
-	return fetch(`${BASE_URL_CURRENT}&q=auto:ip&aqi=yes`);
-};
-
-export const getCurrentCity = (cityName: string) => {
-	console.log("to implement");
-	return fetch(`${BASE_URL_CURRENT}&q=auto:ip&aqi=yes`);
-};
 
 export const getForecastAuto = () => {
 	return fetch(`${BASE_URL_FORECAST}&q=auto:ip&days=3&aqi=no&alerts=no`);
