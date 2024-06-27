83889d2d0aa77a1cdb46cd2b509f32f47282fc00
Valentin Nelu Ifrim <nelu@Valentins-MacBook-Pro.local>
2024-06-27 11:35:16 +0200

    add: android config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.myweather.app',
+  appName: 'my-weather-app',
+  webDir: 'build'
+};
+
+export default config;
